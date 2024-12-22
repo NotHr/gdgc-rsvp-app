5664e1d55dabf24ab540566f0296cb3fec2f006e
Sai Harshith <saiharshith.poli@gmail.com>
2024-12-22 17:45:03 +0530

    excluded static files from middleware

--- middleware.ts
+++ middleware.ts
@@ -16,5 +16,5 @@ export function middleware(request: NextRequest) {
 }
 
 export const config = {
-  matcher: ["/((?!api|login|register).*)"],
+  matcher: ["/((?!api|_next/static|_next/image|favicon.ico|login|register).*)"],
 };
